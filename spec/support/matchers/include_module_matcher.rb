c0d1c378e7fd9ee4c2145ecd8245dfab2fa7277c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 19:18:01 +0530

    feat: added matcher to assert that the module is included within the class

---
+++ spec/support/matchers/include_module_matcher.rb
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# Asserts that the module is included within the class.
+#
+# ```
+# RSpec.describe TableName, type: :model do
+#   it { is_expected.to include_module(ModuleName) }
+# end
+# ```
+RSpec::Matchers.define :include_module do |expected|
+  match do
+    described_class.included_modules.include?(expected)
+  end
+
+  description do
+    "include the #{expected} module"
+  end
+
+  failure_message do
+    "expected #{described_class} to include the #{expected} module"
+  end
+
+  failure_message_when_negated do
+    "expected #{described_class} not to include the #{expected} module"
+  end
+end
