80473f7bbda5b57cc0dbb952bfc718f8e8b516db
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 19:17:34 +0530

    feat: added matcher to assert that the model has a valid factory

---
+++ spec/support/matchers/have_a_valid_factory_matcher.rb
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# Asserts that the model has a valid factory.
+#
+# ```
+# RSpec.describe TableName, type: :model do
+#   it { is_expected.to have_a_valid_factory(factory_name, *traits, **associations) }
+# end
+# ```
+RSpec::Matchers.define :have_a_valid_factory do |factory_name|
+  chain :with_associations do |associations|
+    @associations = associations
+  end
+
+  chain :with_traits do |traits|
+    @traits = traits
+  end
+
+  match do
+    factory = (factory_name || described_class.table_name.singularize.to_sym)
+
+    associations = @associations || {}
+    traits = @traits || []
+
+    FactoryBot.build(factory, *traits, **associations)
+  end
+
+  description do
+    "have valid factory"
+  end
+
+  failure_message do
+    "expected #{described_class} to have valid factory"
+  end
+
+  failure_message_when_negated do
+    "expected #{described_class} not to have valid factory"
+  end
+end
