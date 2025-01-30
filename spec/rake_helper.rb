a0a578982d9c6d3d8b9639d5c11fd740c43a6193
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 11:04:50 +0530

    config: configured `rspec` and `simplecov`

---
+++ spec/rake_helper.rb
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+require "spec_helper"
+
+warn "WARNING: Using `require 'rake_helper'` has no effect. Use `require 'spec_helper'` instead."
