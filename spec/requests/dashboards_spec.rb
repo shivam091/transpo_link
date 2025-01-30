278a16bb8ea40d13349c44f8cd866fc444d06c3e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 20:17:12 +0530

    feat: added shared contexts for login and updated dashboards tests

--- spec/requests/dashboards_spec.rb
+++ spec/requests/dashboards_spec.rb
@@ -7,11 +7,24 @@
 require "spec_helper"
 
 RSpec.describe "Dashboards", type: :request do
-  describe "GET /" do
-    before { get root_path }
 
-    it "returns :ok status" do
-      expect(response).to have_http_status(:ok)
+  context "when user is not logged in" do
+    describe "GET /" do
+      subject { get root_path }
+
+      it { is_expected.to require_login }
+    end
+  end
+
+  context "when user is logged in" do
+    include_context "login as admin"
+
+    describe "GET /" do
+      before { get root_path }
+
+      it "returns :ok status" do
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
