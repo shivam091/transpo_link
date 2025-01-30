5fdd8c8ad50ac3605e9694199b8f4d8ae8a4c08f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 15:43:53 +0530

    feat: added support to display stylish flash messages

---
+++ app/helpers/flash_messages_helper.rb
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+##
+# Helper methods for creating flash messages within the application.
+#
+module FlashMessagesHelper
+  FLASH_TYPES = {
+    notice: {color: :success, icon: "face-smile"},
+    alert: {color: :danger, icon: "face-frown"},
+    warning: {color: :warning, icon: "exclamation-triangle"},
+    info: {color: :info, icon: "info-circle"}
+  }.with_indifferent_access.freeze
+
+  def flash_messages(options = {})
+    flash.map do |msg_type, messages|
+      next if messages.blank?
+
+      next unless FLASH_TYPES.key?(msg_type)
+
+      Array(messages).map { |message| build_flash_message(msg_type, message) }
+    end.flatten.compact.join("\n").html_safe
+  end
+
+  private
+
+  def build_flash_message(msg_type, message)
+    tag.div(class: "alert alert-#{FLASH_TYPES[msg_type][:color]} d-flex align-items-center alert-dismissible fade show") do
+      concat(external_svg_tag("svgs/#{FLASH_TYPES[msg_type][:icon]}.svg", width: "24px", height: "24px", fill: "currentColor", class: "flex-shrink-0 me-2"))
+      concat(tag.div(message))
+      concat(tag.button("", class: "btn-close", "data-bs-dismiss" => "alert", "aria-label" => "Close"))
+    end
+  end
+end
