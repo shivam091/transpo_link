d4ed67a67b3da4e162018e4f1b41e969215ea7cd
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 12:37:02 +0530

    feat: added `dashboards` controller

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class DashboardsController < ApplicationController
+
+  # GET /
+  def show
+  end
+end
