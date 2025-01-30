3dfa904a8fbe7e14fe2af8f6e686ce6938e61b22
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 11:10:23 +0530

    feat: added `favicon` and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@
 
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
+  favicon_redirect = redirect do |_params, _request|
+    ActionController::Base.helpers.asset_url(TranspoLink::Favicon.main)
+  end
+  get "favicon.png", to: favicon_redirect
+  get "favicon.ico", to: favicon_redirect
 end
