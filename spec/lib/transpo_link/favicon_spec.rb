f741a8708daeacd5d5fa42ec358f813fbd8f5593
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 12:45:10 +0530

    test: added specs for `Favicon` module

---
+++ spec/lib/transpo_link/favicon_spec.rb
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/lib/transpo_link/favicon_spec.rb
+
+require "spec_helper"
+
+describe TranspoLink::Favicon do
+  describe ".main" do
+    it "returns the URL of the favicon in development environment" do
+      stub_rails_env("development")
+      expect(described_class.main).to match_asset_path("/assets/favicon.ico")
+    end
+
+    it "returns the URL of the favicon in test environment" do
+      stub_rails_env("test")
+      expect(described_class.main).to match_asset_path("/assets/favicon.ico")
+    end
+
+    it "returns the URL of the favicon in production environment" do
+      stub_rails_env("production")
+      expect(described_class.main).to match_asset_path("/assets/favicon.ico")
+    end
+  end
+end
