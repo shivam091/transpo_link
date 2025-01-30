1089e159c38fb27b0f338034567c2c8acc7b6716
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-30 12:41:39 +0530

    feat: added required helpers for `layout` and `images`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,73 @@
 # -*- warn_indent: true -*-
 
 module ApplicationHelper
+  def render_if_exists(partial, locals: {}, &block)
+    render(partial, locals, &block) if partial_exists?(partial)
+  end
+
+  def partial_exists?(partial)
+    lookup_context.exists?(partial, [], true)
+  end
+
+  def template_exists?(template)
+    lookup_context.exists?(template, [], false)
+  end
+
+  def copyright_year
+    copyright_start_year, copyright_end_year = 2025, Date.current.year
+
+    if copyright_start_year.eql?(copyright_end_year)
+      copyright_end_year
+    else
+      "#{copyright_start_year} - #{copyright_end_year}"
+    end
+  end
+
+  # Returns active css class when condition returns true
+  # otherwise returns nil.
+  #
+  # Example:
+  #   %li{ class: active_when(params[:filter] == '1') }
+  def active_when(condition)
+    "active" if condition
+  end
+
+  # Check if a particular controller is the current one
+  #
+  # args - One or more controller names to check (using path notation when inside namespaces)
+  #
+  # Examples
+  #
+  #   # On OrdersController
+  #   current_controller?(:orders)           # => true
+  #   current_controller?(:invoices)         # => false
+  #
+  #   # On Admin::ProductsController
+  #   current_controller?(:products)         # => true
+  #   current_controller?("products")        # => true
+  def current_controller?(*args)
+    args.any? do |v|
+      TranspoLink::Utils.safe_downcase!(v.to_s).in?([controller.controller_name, controller.controller_path])
+    end
+  end
+
+  # Check if a particular action is the current one
+  #
+  # args - One or more action names to check
+  #
+  # Examples
+  #
+  #   # On Orders#new
+  #   current_action?(:new)           # => true
+  #   current_action?(:create)        # => false
+  #   current_action?(:new, :create)  # => true
+  def current_action?(*args)
+    args.any? { |v| TranspoLink::Utils.safe_downcase!(v.to_s) == action_name }
+  end
+
+  # <% title @post.title %>
+  def title(*text)
+    title_text = [text, t("title")]
+    content_for :title, title_text.join(" &middot; ").html_safe
+  end
 end
---
+++ app/helpers/images_helper.rb
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+##
+# Helper methods for rendering SVG images.
+#
+module ImagesHelper
+  DEFAULT_SVG_OPTIONS = {height: "20px", width: "20px", fill: "currentColor"}.freeze
+
+  def external_svg_tag(file_name, options = {})
+    options = DEFAULT_SVG_OPTIONS.merge(options)
+
+    file_content = File.read(Rails.root.join("app", "assets", "images", file_name))
+    doc = Nokogiri::HTML::DocumentFragment.parse(file_content)
+    svg = doc.at_css("svg")
+
+    options.each do |attr, value|
+      if value.is_a?(Hash)
+        value.transform_keys! { |key| "#{attr}-#{key.to_s.dasherize}" }
+        value.each { |transformed_key, sub_value| svg[transformed_key] = sub_value }
+      else
+        svg[attr.to_s] = value
+      end
+    end
+
+    doc.to_html.html_safe
+  end
+
+  def inline_svg_tag(symbol_id, options = {})
+    options = DEFAULT_SVG_OPTIONS.merge(options)
+    options[:class] = ["icon", "icon-#{symbol_id}", options[:class]].compact.join(" ")
+
+    icon = content_tag(:use, nil, "xlink:href": "#{image_url('svgs/defs.svg')}#icon-#{symbol_id}")
+
+    content_tag(:svg, icon, options)
+  end
+end
