53fa331d9fc2ea3d827105d6061eaa61b27ab4e2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-02 22:55:05 +0530

    test: added test cases for `Profiles#show` and updated login context

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -7,5 +7,23 @@
 require "spec_helper"
 
 RSpec.describe "Profiles", type: :request do
+  context "when user is not logged in" do
+    describe "GET /" do
+      subject { get profile_path }
 
+      it { is_expected.to require_login }
+    end
+  end
+
+  context "when user is logged in" do
+    include_context "login as admin"
+
+    describe "GET /" do
+      before { get profile_path }
+
+      it "returns :ok status" do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
