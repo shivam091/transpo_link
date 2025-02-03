18e9b776107af5a4062c75a65669c117a7fe1c48
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-03 15:43:17 +0530

    feat: added page to display user `preferences`

---
+++ spec/requests/preferences_spec.rb
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/requests/preferences_spec.rb
+
+require "spec_helper"
+
+RSpec.describe "Preferences", type: :request do
+  describe "GET /preference" do
+  end
+end
