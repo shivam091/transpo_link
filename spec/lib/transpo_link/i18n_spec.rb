9e42609b09f158fa28ce46d6cd1253e2fcd36c7f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-03 13:24:35 +0530

    feat: added module containing methods for `i18n`

---
+++ spec/lib/transpo_link/i18n_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/lib/transpo_link/i18n_spec.rb
+
+require "spec_helper"
+
+RSpec.describe TranspoLink::I18n do
+end
