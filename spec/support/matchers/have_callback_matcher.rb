1454a493ed2091ea1c018ecf0ae829a3538510a9
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-10 09:54:28 +0530

    test: added matcher to assert that the specified callback defined within the model

---
+++ spec/support/matchers/have_callback_matcher.rb
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+#  Asserts that the specified callback defined within the model.
+#
+# ```
+# RSpec.describe ModelName, type: :model do
+#   it { is_expected.to have_callback(callback_kind, callback_name, method_name) }
+# end
+# ```
+
+RSpec::Matchers.define :have_callback do |callback_kind, callback_name, method_name|
+  match do |model_class|
+    model_class.__callbacks[callback_name].any? do |cb|
+      cb.kind == callback_kind.to_sym && cb.filter == method_name.to_sym
+    end
+  end
+
+  description do
+    "have a #{callback_kind}_#{callback_name} callback defined for #{method_name}"
+  end
+
+  failure_message do |model_class|
+    "expected to have a #{callback_kind}_#{callback_name} callback defined for #{method_name}"
+  end
+
+  failure_message_when_negated do |model_class|
+    "expected not to have a #{callback_kind}_#{callback_name} callback defined for #{method_name}"
+  end
+end
