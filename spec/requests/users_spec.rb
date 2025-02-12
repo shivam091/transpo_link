6513170ef6a8bf5de1498adab53683da7da5740e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-12 22:31:35 +0530

    test: added tests for user list

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,5 +7,26 @@
 require "spec_helper"
 
 RSpec.describe "Users", type: :request do
+  let!(:admin) { create(:admin, :confirmed, :active) }
 
+  context "when user is not signed in" do
+    describe "GET /users" do
+      subject { get users_path }
+
+      it { is_expected.to require_sign_in }
+    end
+  end
+
+  context "when user is signed in" do
+    include_context "sign in as admin"
+
+    describe "GET /users" do
+      before { get users_path }
+
+      it "renders user list and returns :ok status" do
+        expect(controller_assigns(:users).reload).to include(admin)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
