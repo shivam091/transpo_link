62b694d9e333db193d645411c0ade29e82075694
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-13 12:50:11 +0530

    feat: integrated `bullet` to kill `N+1` queries

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# Be sure to restart your server when you modify this file.
+
+Rails.application.configure do
+  config.after_initialize do
+    if defined?(Bullet)
+      Bullet.enable        = true
+      Bullet.alert         = Rails.env.development?
+      Bullet.bullet_logger = true
+      Bullet.console       = true
+      Bullet.rails_logger  = true
+      Bullet.add_footer    = true
+      Bullet.raise         = Rails.env.test?
+    end
+  end
+end
