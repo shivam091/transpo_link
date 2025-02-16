e543816414a0c16e994fa6af4702bfb0af8b2324
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-16 19:12:23 +0530

    feat: added show page for `request log`

--- app/controllers/request_logs_controller.rb
+++ app/controllers/request_logs_controller.rb
@@ -4,9 +4,21 @@
 
 class RequestLogsController < ApplicationController
 
+  before_action :find_request_log, only: :show
+
   # GET /request-logs
   def index
     @request_logs = RequestLog.includes(user: :user_detail)
     @request_logs, @pagination_data = @request_logs.paginate(params[:page])
+  end
+
+  # GET /request-logs/:id
+  def show
+  end
+
+  private
+
+  def find_request_log
+    @request_log = RequestLog.find(params[:id])
   end
 end
