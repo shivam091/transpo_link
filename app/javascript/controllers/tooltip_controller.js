69bb6bf1fd347e20f3bf716c890c5c820010db2e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-16 14:37:05 +0530

    feat: enabled bootstrap tooltips

---
+++ app/javascript/controllers/tooltip_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  connect() {
+    new bootstrap.Tooltip(this.element, {
+      html: true,
+      trigger: "hover"
+    });
+  }
+}
