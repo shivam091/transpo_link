316b01f6d662d488fdcd5c8511113b9c81618173
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-20 16:32:00 +0530

    test: added tests for `Warehouses::DestroyService`

---
+++ spec/services/warehouses/destroy_service_spec.rb
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/warehouses/destroy_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Warehouses::DestroyService, type: :service do
+  let!(:warehouse) { create(:warehouse) }
+  subject { described_class.(warehouse) }
+
+  describe "#call" do
+    context "when destroy is successful" do
+      include_examples "deletes an object", Warehouse
+
+      it "sets flash mmesage" do
+        expect(subject.message).to eq("Warehouse was successfully deleted.")
+        expect(Warehouse.find_by(id: warehouse.id)).to be_nil
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when destroy fails" do
+      before { allow(warehouse).to receive(:destroy).and_return(false) }
+
+      include_examples "does not change count of objects", Warehouse
+
+      it "sets flash mmesage" do
+        expect(subject.message).to eq("Warehouse could not be deleted.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
+end
