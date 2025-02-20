84567d6fd8fc97a3110d8342744cd58010ad9453
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-20 16:32:32 +0530

    test: added shared example for to assert object is new record

---
+++ spec/support/shared_examples/initializes_a_new_instance.rb
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+RSpec.shared_examples "initializes a new instance" do |object_name, klass|
+  it "initializes a new #{klass.model_name}" do
+    expect(controller_assigns(object_name)).to be_a_new(klass)
+  end
+end
