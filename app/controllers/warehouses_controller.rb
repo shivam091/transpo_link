9c948fde81f3d45c08f4cf86ae3ff3db1be01070
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-20 13:26:20 +0530

    feat: added feature to create the `warehouse`

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -9,4 +9,45 @@ class WarehousesController < ApplicationController
     @warehouses = Warehouse.all
     @warehouses, @pagination_data = @warehouses.paginate(page: params[:page])
   end
+
+  # GET /warehouses/new
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  # POST /warehouses
+  def create
+    response = Warehouses::CreateService.(warehouse_params)
+    @warehouse = response.payload[:warehouse]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to warehouses_path, status: :see_other
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:warehouse_form, partial: "warehouses/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(
+      :name,
+      :email_address,
+      :contact_number,
+      :description,
+      :total_capacity,
+      :capacity_unit,
+      :latitude,
+      :longitude,
+      :is_active
+    )
+  end
 end
