4d0f1580a433e09c747c834e90eca2ccd94e0856
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-21 12:38:00 +0530

    test: updated tests for `PaginationHelper`

--- spec/helpers/pagination_helper_spec.rb
+++ spec/helpers/pagination_helper_spec.rb
@@ -54,10 +54,54 @@ RSpec.describe PaginationHelper, type: :helper do
       expect(helper.render_pagination({total_pages: 1})).to eq("")
     end
 
-    it "renders pagination navigation" do
+    it "renders pagination" do
       rendered_html = helper.render_pagination(pagination_data)
       expect(rendered_html).to include("<nav aria-label=\"Pagination\">")
-      expect(rendered_html).to include("<ul class=\"pagination justify-content-center\">")
+      expect(rendered_html).to include("<p class=\"mb-0\">")
+    end
+  end
+
+  describe "#record_info_tag" do
+    it "renders record info correctly for middle page" do
+      html = helper.send(:record_info_tag, pagination_data)
+      expect(html).to include("Showing 21 to 30 of 50 entries")
+    end
+
+    it "renders correct info for first page" do
+      html = helper.send(:record_info_tag, pagination_data_first_page)
+      expect(html).to include("Showing 1 to 10 of 50 entries")
+    end
+
+    it "renders correct info for last page" do
+      html = helper.send(:record_info_tag, pagination_data_last_page)
+      expect(html).to include("Showing 41 to 50 of 50 entries")
+    end
+
+    it "renders zero entries when no records exist" do
+      html = helper.send(:record_info_tag, {current_page: 1, per_page: 10, total_entries: 0})
+      expect(html).to include("Showing 1 to 0 of 0 entries")
+    end
+  end
+
+  describe "#pagination_nav_tag" do
+    it "renders pagination navigation correctly" do
+      html = helper.send(:pagination_nav_tag, pagination_data)
+      expect(html).to include("<nav aria-label=\"Pagination\">")
+      expect(html).to include("<ul class=\"pagination mb-0\">")
+      expect(html).to include("page=2")
+      expect(html).to include("page=4")
+    end
+
+    it "does not include previous page link if on first page" do
+      html = helper.send(:pagination_nav_tag, pagination_data_first_page)
+      expect(html).to include("class=\"page-item disabled\"")
+      expect(html).not_to include("page=0")
+    end
+
+    it "does not include next page link if on last page" do
+      html = helper.send(:pagination_nav_tag, pagination_data_last_page)
+      expect(html).to include("class=\"page-item disabled\"")
+      expect(html).not_to include("page=6")
     end
   end
 
@@ -98,6 +142,45 @@ RSpec.describe PaginationHelper, type: :helper do
     it "renders ellipsis when total pages exceed 8" do
       html = helper.send(:page_number_tags, {current_page: 5, total_pages: 10})
       expect(html).to include("...")
+    end
+  end
+
+  describe "#full_page_range" do
+    it "renders all pages when total pages are small" do
+      html = helper.send(:full_page_range, 1, 5)
+      (2..5).each { |page| expect(html).to include("page=#{page}") }
+    end
+  end
+
+  describe "#generate_page_items" do
+    it "renders page items with ellipsis correctly" do
+      pages = [1, "...", 5]
+      html = helper.send(:generate_page_items, pages, 3)
+      expect(html).to include("...")
+      expect(html).to include("page=1")
+      expect(html).to include("page=5")
+    end
+  end
+
+  describe "#disabled_page_ellipsis" do
+    it "renders a disabled page item for ellipsis" do
+      html = helper.send(:disabled_page_ellipsis)
+      expect(html).to include("class=\"page-item disabled\"")
+      expect(html).to include("<span class=\"page-link\">...</span>")
+    end
+  end
+
+  describe "#page_item" do
+    it "renders an active page item for the current page" do
+      html = helper.send(:page_item, 3, 3)
+      expect(html).to include("class=\"page-item active\"")
+      expect(html).to include("<span class=\"page-link\">3</span>")
+    end
+
+    it "renders a clickable page item for other pages" do
+      html = helper.send(:page_item, 2, 3)
+      expect(html).to include("page=2")
+      expect(html).to include("class=\"page-item \"")
     end
   end
 end
