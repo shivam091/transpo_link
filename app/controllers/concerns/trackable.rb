60428df3183de27c9709f5dd1eb4eba96336ce23
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-22 21:58:36 +0530

    refactor: changes in `Trackable` to track failed requests

--- app/controllers/concerns/trackable.rb
+++ app/controllers/concerns/trackable.rb
@@ -7,31 +7,46 @@ module Trackable
   extend ActiveSupport::Concern
 
   included do
-    after_action :log_activity
+    around_action :log_activity
   end
 
   private
 
   def log_activity
-    log_data = {
+    @request_log = RequestLog.new(
       uuid: request.uuid,
       uri: "#{request.protocol}#{request.host_with_port}#{request.path}",
       method: request.request_method,
       session_id: request.session.id.to_s,
       session_private_id: request.session.id&.private_id.to_s,
       remote_address: IPAddr.new(request.remote_ip),
-      elapsed_time: (Time.now.utc - request.env["REQUEST_STARTED_AT"]),
       user_agent: request.user_agent,
       referrer: request.referrer,
-      exception_message: request.env["rack.exception"]&.message,
-      status: response.status,
-      response_size: response.body.bytesize,
       query_params: request.filtered_parameters.except(:controller, :action),
       ip_info: request.env["ipinfo"],
       user: current_user
-    }
-    RequestLog.create!(log_data)
-  rescue StandardError => e
-    Rails.logger.error("Failed to log request: #{e.message}")
+    )
+    yield
+  rescue StandardError => exception
+    request.env["rack.exception"] = exception
+    raise exception
+  ensure
+    exception = request.env["rack.exception"]
+
+    @request_log.assign_attributes(
+      exception_message: exception&.message,
+      elapsed_time: (Time.now.utc - request.env["REQUEST_STARTED_AT"]),
+      response_size: (response.body&.bytesize || 0),
+      status: (exception ? http_status_for(exception) : (response&.status || 500))
+    )
+    @request_log.save!
+  end
+
+  def http_status_for(exception)
+    case exception
+    when ActiveRecord::RecordNotFound, ActionController::RoutingError then 404
+    when ActionController::InvalidAuthenticityToken                   then 403
+    else                                                                   500
+    end
   end
 end
