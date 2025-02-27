907f453e81f22c55cf0300ddba26756c03a9ac31
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-27 22:06:46 +0530

    feat(taxes): added model `TaxDetail` & association

---
+++ app/models/tax_detail.rb
@@ -0,0 +1,108 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class TaxDetail < ApplicationRecord
+  include Sortable, Pageable
+
+  enum :tax_type, {
+    # Global
+    vat: "vat",                           # Value Added Tax
+    gst: "gst",                           # Goods and Services Tax
+    tin: "tin",                           # Taxpayer Identification Number
+
+    # United States
+    ein: "ein",                           # Employer Identification Number
+    ssn: "ssn",                           # Social Security Number
+    itin: "itin",                         # Individual Taxpayer Identification Number
+
+    # India
+    pan: "pan",                           # Permanent Account Number
+    tan: "tan",                           # Tax Deduction and Collection Account Number
+    gstin: "gstin",                       # GST Identification Number
+
+    # European Union (EU)
+    vatin: "vatin",                       # VAT Identification Number
+    eori: "eori",                         # Economic Operators Registration and Identification Number
+    nif: "nif",                           # Tax Identification Number
+    cif: "cif",                           # Tax Identification Code
+    siret: "siret",                       # Business Identification System
+    siren: "siren",                       # Establishment Identification System
+
+    # United Kingdom
+    utr: "utr",                           # Unique Taxpayer Reference
+
+    # Canada
+    bn: "bn",                             # Business Number
+    qst: "qst",                           # Quebec Sales Tax
+
+    # Australia & New Zealand
+    abn: "abn",                           # Australian Business Number
+    acn: "acn",                           # Australian Company Number
+    tfn: "tfn",                           # Tax File Number
+    ird: "ird",                           # Inland Revenue Department Number
+
+    # Latin America
+    rfc: "rfc",                           # Federal Taxpayer Registry
+    cuit: "cuit",                         # Unique Tax Identification Code
+    cuil: "cuil",                         # Unique Labor Identification Code
+    ruc: "ruc",                           # Single Taxpayer Registry
+    nit: "nit",                           # Tax Identification Number
+
+    # Brazil
+    cnpj: "cnpj",                         # National Register of Legal Entities
+    cpf: "cpf",                           # Register of Natural Persons
+
+    # Indonesia
+    npwp: "npwp",                         # Taxpayer Identification Number
+
+    # Middle East & Africa
+    trn: "trn",                           # Tax Registration Number
+    kra_pin: "kra_pin",                   # Kenya Revenue Authority PIN
+
+    # Hong Kong & Malaysia
+    brn: "brn",                           # Business Registration Number
+
+    # Japan
+    corporate_number: "corporate_number", # Corporate Number
+    my_number: "my_number",               # Individual Tax Number
+
+    # Russia
+    inn: "inn",                           # Taxpayer Identification Number
+    kpp: "kpp",                           # Tax Registration Reason Code
+    ogrn: "ogrn",                         # Principal State Registration Number
+    ogrnip: "ogrnip",                     # Individual Entrepreneur Registration Number
+
+    # South Korea
+    brn_kr: "brn_kr",                     # Business Registration Number
+
+    # China
+    uscc: "uscc",                         # Unified Social Credit Code
+
+    # Vietnam
+    mst: "mst",                           # Tax Identification Number
+
+    # Philippines
+    tin_ph: "tin_ph",                     # Taxpayer Identification Number
+
+    # Thailand
+    tin_th: "tin_th",                     # Taxpayer Identification Number
+
+    # Singapore
+    uen: "uen",                           # Unique Entity Number
+  }
+
+  INTERNATIONAL_TAX_TYPES = %w[vat gst tin].freeze
+
+  REGIONAL_TAX_TYPES = %w[vatin eori].freeze
+
+  NATIONAL_TAX_TYPES = %w[
+    ein ssn itin pan tan gstin nif cif siret siren utr bn qst abn acn tfn ird rfc
+    cuit cuil cnpj cpf npwp trn kra_pin corporate_number my_number inn kpp ogrn
+    ogrnip brn_kr uscc mst tin_ph tin_th uen
+  ].freeze
+
+  COUNTRY_REQUIRING_TAX_TYPES = INTERNATIONAL_TAX_TYPES + REGIONAL_TAX_TYPES + %w[ruc nit brn].freeze
+
+  belongs_to :user, inverse_of: :tax_details, touch: true
+end
---
+++ spec/models/tax_detail_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/models/tax_detail_spec.rb
+
+require "spec_helper"
+
+RSpec.describe TaxDetail, type: :model do
+end
