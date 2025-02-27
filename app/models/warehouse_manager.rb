b96130b6d5a63ceaf36b6b75515b7a6cd5f9606d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-27 18:13:34 +0530

    refactor: removed unneeded validations
    removed unneeded validations from `WarehouseManager` and `WarehouseSupplier` models

--- app/models/warehouse_manager.rb
+++ app/models/warehouse_manager.rb
@@ -3,8 +3,6 @@
 # -*- warn_indent: true -*-
 
 class WarehouseManager < ApplicationRecord
-  validates :warehouse_id, :manager_id, presence: true, reduce: true
-
   belongs_to :warehouse, inverse_of: :warehouse_managers, touch: true
   belongs_to :manager, inverse_of: :warehouse_managers, class_name: "User"
 end
--- app/models/warehouse_supplier.rb
+++ app/models/warehouse_supplier.rb
@@ -3,8 +3,6 @@
 # -*- warn_indent: true -*-
 
 class WarehouseSupplier < ApplicationRecord
-  validates :warehouse_id, :supplier_id, presence: true, reduce: true
-
   belongs_to :warehouse, inverse_of: :warehouse_suppliers, touch: true
   belongs_to :supplier, inverse_of: :warehouse_suppliers, class_name: "User"
 end
