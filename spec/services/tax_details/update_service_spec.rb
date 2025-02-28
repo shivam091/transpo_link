1b18c8d9d0c48771e38c0e76d7cd4c090ac24f32
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-02-28 16:44:41 +0530

    test(taxes): added tests for service classes

--- spec/services/tax_details/update_service_spec.rb
+++ spec/services/tax_details/update_service_spec.rb
@@ -7,4 +7,29 @@
 require "spec_helper"
 
 RSpec.describe TaxDetails::UpdateService, type: :service do
+  let(:tax_detail) { create(:tax_detail) }
+  let(:tax_detail_attributes) { attributes_for(:tax_detail, tax_type: "gstin") }
+  subject { described_class.(tax_detail, tax_detail_attributes) }
+
+  describe "#call" do
+    context "when update is successful" do
+      it "updates the tax detail" do
+        expect(subject.payload[:tax_detail].tax_type).to eq("gstin")
+        expect(subject.message).to eq("Tax detail was successfully updated.")
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when update fails" do
+      before { allow(tax_detail).to receive(:update) { false } }
+
+      it "does not update the tax detail" do
+        expect(subject.payload[:tax_detail].tax_type).to eq("pan")
+        expect(subject.message).to eq("Tax detail could not be updated.")
+      end
+
+      include_examples "returns an error response"
+    end
+  end
 end
