eefe504ae6eff4a7649b864ff230469240a258bf
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-01 23:11:17 +0530

    test(taxes): added tests for `TaxRate` model

--- spec/factories/tax_rates.rb
+++ spec/factories/tax_rates.rb
@@ -4,6 +4,10 @@
 
 FactoryBot.define do
   factory :tax_rate do
-
+    country { "IN" }
+    tax_type { "gstin" }
+    rate { 8.0 }
+    valid_from { Date.current }
+    valid_to { Date.current + 1.year }
   end
 end
--- spec/models/tax_rate_spec.rb
+++ spec/models/tax_rate_spec.rb
@@ -7,4 +7,41 @@
 require "spec_helper"
 
 RSpec.describe TaxRate, type: :model do
+  subject { build(:tax_rate) }
+
+  describe "valid factory" do
+    it { is_expected.to have_a_valid_factory(:tax_rate) }
+  end
+
+  describe "attributes, indexes, foreign keys, and check constraints" do
+    it { is_expected.to have_db_column(:id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:country).of_type(:string) }
+    it { is_expected.to have_db_column(:tax_type).of_type(:enum) }
+    it { is_expected.to have_db_column(:rate).of_type(:decimal).with_options(precision: 5, scale: 2) }
+    it { is_expected.to have_db_column(:valid_from).of_type(:date) }
+    it { is_expected.to have_db_column(:valid_to).of_type(:date) }
+    it { is_expected.to have_db_column(:created_at).of_type(:timestamptz).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:timestamptz).with_options(null: false) }
+
+    it { is_expected.to have_db_index(:valid_from) }
+    it { is_expected.to have_db_index(:valid_to) }
+    it { is_expected.to have_db_index([:country, :tax_type]) }
+    it { is_expected.to have_db_index([:country, :tax_type, :valid_from, :valid_to]).unique(true) }
+
+    it { is_expected.to have_check_constraint(:check_tax_rates_country_presence).with_expression("country IS NOT NULL AND country::text <> ''::text") }
+    it { is_expected.to have_check_constraint(:check_tax_rates_tax_type_presence).with_expression("tax_type IS NOT NULL") }
+    it { is_expected.to have_check_constraint(:check_tax_rates_rate_presence).with_expression("rate IS NOT NULL") }
+    it { is_expected.to have_check_constraint(:check_tax_rates_rate_numericality).with_expression("rate >= 0::numeric AND rate <= 100::numeric") }
+    it { is_expected.to have_check_constraint(:check_tax_rates_valid_from_presence).with_expression("valid_from IS NOT NULL") }
+    it { is_expected.to have_check_constraint(:check_tax_rates_valid_from_future).with_expression("valid_from >= CURRENT_DATE") }
+    it { is_expected.to have_check_constraint(:check_tax_rates_valid_from_to_comparison).with_expression("valid_to IS NULL OR valid_to > valid_from") }
+  end
+
+  it_behaves_like "tax type"
+
+  describe "included modules" do
+    it { is_expected.to include_module(Sortable) }
+    it { is_expected.to include_module(Pageable) }
+    it { is_expected.to include_module(Taxable) }
+  end
 end
