cff567d8a191f4effce10e066b3c34f4081eca05
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-02 18:18:09 +0530

    test(taxes): updated traits and factory

--- spec/factories/tax_rates.rb
+++ spec/factories/tax_rates.rb
@@ -6,13 +6,10 @@ FactoryBot.define do
   factory :tax_rate do
     country { "DE" }
     tax_type { "vat" }
+    business_category { "b2b" }
     rate { 8.0 }
     valid_from { Date.current }
-    valid_to { Date.current + 1.year }
-  end
-
-  trait :for_b2b do
-    business_category { "b2b" }
+    valid_to { Date.current + 1.month }
   end
 
   trait :for_b2c do
