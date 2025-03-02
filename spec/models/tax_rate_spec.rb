2022d1d9af17a5aa4ad7730ff9f4a640b9d984e2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-02 18:22:05 +0530

    test(taxes): added tests for `TaxRate`
    added tests for `scopes`, `methods`, and `validations`

--- spec/models/tax_rate_spec.rb
+++ spec/models/tax_rate_spec.rb
@@ -7,7 +7,7 @@
 require "spec_helper"
 
 RSpec.describe TaxRate, type: :model do
-  subject { build(:tax_rate) }
+  subject(:tax_rate) { build(:tax_rate) }
 
   describe "valid factory" do
     it { is_expected.to have_a_valid_factory(:tax_rate) }
@@ -49,9 +49,167 @@ RSpec.describe TaxRate, type: :model do
 
   it_behaves_like "tax type"
 
+  describe "enums" do
+    it { is_expected.to define_enum_for(:business_category).with_values({b2b: "b2b", b2c: "b2c"}).backed_by_column_of_type(:enum) }
+  end
+
+  describe "default values" do
+    it "should set b2b as default value for #business_category" do
+      expect(tax_rate.business_category).to eq("b2b")
+    end
+  end
+
   describe "included modules" do
     it { is_expected.to include_module(Sortable) }
     it { is_expected.to include_module(Pageable) }
     it { is_expected.to include_module(Taxable) }
+  end
+
+  describe "validations" do
+    describe "#tax_type" do
+      # it do
+      #   is_expected.to validate_uniqueness_of(:tax_type)
+      #     .scoped_to([:country, :business_category, :valid_from])
+      #     .with_message("already exist for this country and business category for selected date")
+      #     .case_insensitive
+      # end
+    end
+
+    describe "#business_category" do
+      it { is_expected.to validate_presence_of(:business_category) }
+      # it { is_expected.to validate_inclusion_of(:business_category).in_array(described_class.business_categories.values) }
+    end
+
+    describe "#rate" do
+      it { is_expected.to validate_presence_of(:rate) }
+      it { is_expected.to validate_numericality_of(:rate).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(100) }
+    end
+
+    describe "#valid_from" do
+      it { is_expected.to validate_presence_of(:valid_from) }
+      it { is_expected.to validate_comparison_of(:valid_from).is_greater_than_or_equal_to(Date.current).with_message("must be today or a future date") }
+    end
+
+    describe "#valid_to" do
+      it { is_expected.to validate_comparison_of(:valid_to).is_greater_than(:valid_from) }
+    end
+  end
+
+  describe "scopes" do
+    let!(:active_tax_rate) { create(:tax_rate, :gstin) }
+    let!(:future_tax_rate) { create(:tax_rate, :for_b2c, country: "IN", valid_from: Date.current + 1.year, valid_to: Date.current + 5.years) }
+
+    describe ".active" do
+      it "returns only active tax rates" do
+        expect(described_class.active).to include(active_tax_rate)
+        expect(described_class.active).not_to include(future_tax_rate)
+      end
+    end
+
+    describe ".for_country" do
+      it "returns tax rates for the given country" do
+        expect(described_class.for_country("IN")).to include(future_tax_rate)
+        expect(described_class.for_country("DE")).not_to include(future_tax_rate)
+      end
+    end
+
+    describe ".for_tax_type" do
+      it "returns tax rates for the given tax type" do
+        expect(described_class.for_tax_type("gstin")).to include(active_tax_rate)
+        expect(described_class.for_tax_type("vat")).not_to include(active_tax_rate)
+      end
+    end
+
+    describe ".for_category" do
+      it "returns tax rates for the given category" do
+        expect(described_class.for_category("b2c")).to include(future_tax_rate)
+        expect(described_class.for_category("b2b")).not_to include(future_tax_rate)
+      end
+    end
+
+    describe ".applicable_rates" do
+      it "returns applicable tax rates matching tax type, country, and category" do
+        expect(described_class.applicable_rates("gstin", "DE", "b2b")).to include(active_tax_rate)
+        expect(described_class.applicable_rates("gstin", "DE", "b2c")).not_to include(active_tax_rate)
+      end
+    end
+
+    describe ".valid_on" do
+      it "returns tax rates valid on given date" do
+        expect(described_class.valid_on(Date.current + 1.year)).not_to include(active_tax_rate)
+        expect(described_class.valid_on(Date.current + 1.year)).to include(future_tax_rate)
+      end
+    end
+
+    include_examples "apply default scope on created_at:desc"
+  end
+
+  describe "class methods" do
+    let!(:active_tax_rate) { create(:tax_rate, :gstin) }
+    let!(:future_tax_rate) { create(:tax_rate, :for_b2c, country: "IN", valid_from: Date.current + 1.year, valid_to: Date.current + 5.years) }
+
+    describe ".active_rate" do
+      it "returns the tax rate for a current date" do
+        expect(described_class.active_rate("DE", "gstin")).to eq(active_tax_rate)
+        expect(described_class.active_rate("DE", "gstin")).not_to eq(future_tax_rate)
+      end
+    end
+
+    describe ".future_rate" do
+      it "returns the tax rate for a future date" do
+        expect(described_class.future_rate("IN", "vat", Date.current + 1.year)).to eq(future_tax_rate)
+        expect(described_class.future_rate("IN", "vat", Date.current + 1.year)).not_to eq(active_tax_rate)
+      end
+    end
+  end
+
+  describe "instance methods" do
+    describe "#no_overlapping_tax_rates" do
+      let!(:existing_tax_rate) { create(:tax_rate) }
+
+      context "when creating a tax rate with overlapping valid dates" do
+        let(:new_tax_rate) { build(:tax_rate, valid_from: Date.current + 1.day, valid_to: Date.current + 2.years) }
+
+        it "is not valid" do
+          expect(new_tax_rate).not_to be_valid
+          expect(new_tax_rate.errors[:base]).to include("There is already an active tax rate for this country, tax type, and business category in the selected date range")
+        end
+      end
+
+      context "when creating a tax rate with overlapping valid dates but with other business category" do
+        let(:new_tax_rate) { build(:tax_rate, business_category: "b2c") }
+
+        it { expect(new_tax_rate).to be_valid }
+      end
+
+      context "when creating a tax rate with non-overlapping valid dates" do
+        let(:new_tax_rate) { build(:tax_rate, valid_from: Date.current + 2.years, valid_to: Date.current + 3.years) }
+
+        it { expect(new_tax_rate).to be_valid }
+      end
+    end
+
+    describe "#cannot_change_rate_for_active_tax_rate" do
+      context "when updating the rate of an active tax rate" do
+        let!(:active_tax_rate) { create(:tax_rate) }
+
+        it "is not valid" do
+          active_tax_rate.rate = 10.0
+
+          expect(active_tax_rate).not_to be_valid
+          expect(active_tax_rate.errors[:rate]).to include("cannot be changed for an active tax rate")
+        end
+      end
+
+      context "when updating the rate before the tax rate becomes active" do
+        let(:future_tax_rate) { create(:tax_rate, valid_from: Date.current + 1.month, valid_to: Date.current + 2.year) }
+
+        it "is valid" do
+          future_tax_rate.rate = 10.0
+
+          expect(future_tax_rate).to be_valid
+        end
+      end
+    end
   end
 end
