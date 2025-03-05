876338161329e4904aa2cbfd15cbff5816f1a4d4
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-05 16:07:04 +0530

    feat(regex): updated email regex and test cases

--- lib/transpo_link/regex.rb
+++ lib/transpo_link/regex.rb
@@ -5,6 +5,6 @@
 module TranspoLink
   module Regex
     STRONG_PASSWORD_REGEX = /\A^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$\z/.freeze
-    EMAIL_REGEX = /^[a-zA-Z0-9_\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/i.freeze
+    EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}\z/.freeze
   end
 end
