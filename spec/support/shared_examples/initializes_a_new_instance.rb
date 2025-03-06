a9929a20c8171918f6281741606d9de80c60e8e0
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-06 16:25:08 +0530

    style(tests): updated shared example
    updated `initializes a new instance` shared example to humanize model name

--- spec/support/shared_examples/initializes_a_new_instance.rb
+++ spec/support/shared_examples/initializes_a_new_instance.rb
@@ -3,7 +3,7 @@
 # -*- warn_indent: true -*-
 
 RSpec.shared_examples "initializes a new instance" do |object_name, klass|
-  it "initializes a new #{klass.model_name.name.downcase}" do
+  it "initializes a new #{klass.model_name.human.downcase}" do
     expect(controller_assigns(object_name)).to be_a_new(klass)
     expect(response).to have_http_status(:ok)
   end
