8d8ed782807f901266165d20e584c5f83f747219
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-07 14:46:07 +0530

    feat(category): added service to destroy `category`

---
+++ app/services/product_categories/destroy_service.rb
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class ProductCategories::DestroyService < ApplicationService
+  def initialize(product_category)
+    @product_category = product_category
+  end
+
+  def call
+    destroy_product_category
+  end
+
+  private
+
+  attr_reader :product_category
+
+  def destroy_product_category
+    if product_category.destroy
+      ServiceResponse.success(
+        message: t("product_categories.destroy.info"),
+        payload: {product_category:}
+      )
+    else
+      ServiceResponse.error(
+        message: t("product_categories.destroy.alert"),
+        payload: {product_category:}
+      )
+    end
+  end
+end
