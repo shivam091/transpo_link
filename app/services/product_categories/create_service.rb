efe598486400aef0d9d19e537634f6912b956cee
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-07 14:45:51 +0530

    feat(category): added service to create `category`

---
+++ app/services/product_categories/create_service.rb
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class ProductCategories::CreateService < ApplicationService
+  def initialize(product_category_attributes)
+    @product_category_attributes = product_category_attributes
+  end
+
+  def call
+    create_product_category
+  end
+
+  private
+
+  attr_reader :product_category_attributes
+
+  def create_product_category
+    product_category = ProductCategory.new(product_category_attributes)
+
+    if product_category.save
+      ServiceResponse.success(
+        message: t("product_categories.create.notice"),
+        payload: {product_category:}
+      )
+    else
+      ServiceResponse.error(
+        message: t("product_categories.create.alert"),
+        payload: {product_category:}
+      )
+    end
+  end
+end
