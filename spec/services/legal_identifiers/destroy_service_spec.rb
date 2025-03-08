be973682dc1a43dbd63887e7d8ae7586529e29f9
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-08 16:02:07 +0530

    refactor(tests): refactored service specs

--- spec/services/legal_identifiers/destroy_service_spec.rb
+++ spec/services/legal_identifiers/destroy_service_spec.rb
@@ -8,29 +8,19 @@ require "spec_helper"
 
 RSpec.describe LegalIdentifiers::DestroyService, type: :service do
   let!(:legal_identifier) { create(:legal_identifier) }
-  subject { described_class.(legal_identifier) }
 
-  describe "#call" do
+  subject(:service_response) { described_class.(legal_identifier) }
+
+  describe ".call" do
     context "when destroy is successful" do
-      include_examples "deletes an object", LegalIdentifier
-
-      it "sets flash message" do
-        expect(subject.message).to eq("Legal identifier was successfully deleted.")
-        expect(LegalIdentifier.find_by(id: legal_identifier.id)).to be_nil
-      end
-
+      include_examples "deletes a record", LegalIdentifier
       include_examples "returns a success response"
     end
 
     context "when destroy fails" do
       before { allow(legal_identifier).to receive(:destroy) { false } }
 
-      include_examples "does not change count of objects", LegalIdentifier
-
-      it "sets flash message" do
-        expect(subject.message).to eq("Legal identifier could not be deleted.")
-      end
-
+      include_examples "does not change record count", LegalIdentifier
       include_examples "returns an error response"
     end
   end
