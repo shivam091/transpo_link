ba0becf54586ec6eb4c5077ab0318b5bb4aa394f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-13 15:24:21 +0530

    style: symbolic names for fk and chk constraints

--- db/migrate/20250201103133_create_user_preferences.rb
+++ db/migrate/20250201103133_create_user_preferences.rb
@@ -11,12 +11,12 @@ class CreateUserPreferences < ActiveRecord::Migration[8.0]
                    type: :uuid,
                    foreign_key: {
                      to_table: :users,
-                     name: "fk_user_preferences_user_id_on_users",
+                     name: :fk_user_preferences_user_id_on_users,
                      on_delete: :cascade
                    },
                    null: false,
                    primary_key: true,
-                   index: {using: "btree", unique: true}
+                   index: {using: :btree, unique: true}
       t.string :preferred_locale
       t.string :preferred_time_zone
       t.string :preferred_currency
@@ -24,11 +24,11 @@ class CreateUserPreferences < ActiveRecord::Migration[8.0]
       t.boolean :are_notifications_enabled
       t.timestamps_with_timezone null: false
 
-      t.check_constraint "preferred_locale IS NOT NULL AND preferred_locale  <> ''", name: "check_user_preferences_preferred_locale_presence"
-      t.check_constraint "preferred_time_zone IS NOT NULL AND preferred_time_zone  <> ''", name: "check_user_preferences_preferred_time_zone_presence"
-      t.check_constraint "preferred_currency IS NOT NULL AND preferred_currency  <> ''", name: "check_user_preferences_preferred_currency_presence"
-      t.check_constraint "preferred_color_scheme IS NOT NULL", name: "check_user_preferences_preferred_color_scheme_presence"
-      t.check_constraint "preferred_color_scheme IN (#{enum_values('color_schemes')})", name: "check_user_preferences_preferred_color_scheme_inclusion"
+      t.check_constraint "preferred_locale IS NOT NULL AND preferred_locale  <> ''", name: :check_user_preferences_preferred_locale_presence
+      t.check_constraint "preferred_time_zone IS NOT NULL AND preferred_time_zone  <> ''", name: :check_user_preferences_preferred_time_zone_presence
+      t.check_constraint "preferred_currency IS NOT NULL AND preferred_currency  <> ''", name: :check_user_preferences_preferred_currency_presence
+      t.check_constraint "preferred_color_scheme IS NOT NULL", name: :check_user_preferences_preferred_color_scheme_presence
+      t.check_constraint "preferred_color_scheme IN (#{enum_values('color_schemes')})", name: :check_user_preferences_preferred_color_scheme_inclusion
     end
   end
 end
