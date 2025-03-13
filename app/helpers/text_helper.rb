0037cac9567e5a5bb62f4995b284b6cc26e38b1e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-13 19:36:27 +0530

    feat(helpers): added helper `#word_wrap`
    added helper method `#word_wrap` to wrap long text at specified `line_width` value using `seperator`

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -3,6 +3,20 @@
 # -*- warn_indent: true -*-
 
 module TextHelper
+  def word_wrap(text, options = {})
+    options = {wrap_length: 80, seperator: "<br/>"}.merge(options)
+
+    # Return the text if it's shorter than the line width
+    return text if text.length <= options[:wrap_length]
+
+    # Break text into words
+    wrapped_text = text.scan(/\S.{0,#{options[:wrap_length] - 1}}\S(?=\s|$)|\S+/).join(" ")
+    # Break lines at the desired width and insert separator
+    wrapped_text = wrapped_text.scan(/.{1,#{options[:wrap_length]}}/).join(options[:seperator])
+
+    wrapped_text
+  end
+
   def truncate(text, options = {})
     options = {truncate_at: 30, omission: "..."}.merge(options)
 
--- spec/helpers/text_helper_spec.rb
+++ spec/helpers/text_helper_spec.rb
@@ -7,6 +7,65 @@
 require "spec_helper"
 
 RSpec.describe TextHelper, type: :helper do
+  describe "#word_wrap" do
+    let!(:short_text) { "Short text." }
+    let!(:long_text) { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam." }
+    let!(:mixed_text) { "Lorem ipsum dolor sit amet, consectetur adipiscing elit." }
+
+    it "wraps text to the specified line width" do
+      wrapped_text = helper.word_wrap(long_text, wrap_length: 20, seperator: "<br/>")
+
+      # Check if lines break at the correct position (within 20 characters)
+      expect(wrapped_text.split("<br/>").all? { |line| line.length <= 20 }).to be_truthy
+    end
+
+    it "wraps short text without changes" do
+      wrapped_text = helper.word_wrap(short_text, wrap_length: 20, seperator: "<br/>")
+
+      expect(wrapped_text).to eq(short_text)
+    end
+
+    it "wraps text with long words that exceed the line width" do
+      wrapped_text = helper.word_wrap(long_text, wrap_length: 10, seperator: "<br/>")
+
+      # Check if words are broken into smaller pieces when exceeding the line width
+      expect(wrapped_text).to match(/.{1,10}\s+/)
+    end
+
+    it "does not wrap if the text fits within the line width" do
+      wrapped_text = helper.word_wrap(mixed_text, wrap_length: 80, seperator: "<br/>")
+
+      expect(wrapped_text).to eq(mixed_text)
+    end
+
+    it "uses the custom separator" do
+      wrapped_text = helper.word_wrap(long_text, wrap_length: 20, seperator: "<hr/>")
+
+      # Check if the separator is used to split the wrapped lines
+      expect(wrapped_text).to include("<hr/>")
+    end
+
+    it "handles line breaks correctly even with multiple spaces" do
+      wrapped_text = helper.word_wrap("Lorem ipsum   dolor   sit amet.", wrap_length: 10, seperator: "<br/>")
+
+      expect(wrapped_text).to include("<br/>")
+      expect(wrapped_text.split("<br/>").all? { |line| line.length <= 10 }).to be true
+    end
+
+    it "wraps text even when the line width is smaller than the longest word" do
+      wrapped_text = helper.word_wrap("abcdefghij", wrap_length: 5, seperator: "<br/>")
+
+      # Word 'abcdefghij' should be wrapped because wrap_length is smaller than the word length
+      expect(wrapped_text).to include("<br/>")
+    end
+
+    it "handles empty text gracefully" do
+      wrapped_text = helper.word_wrap("", wrap_length: 10, seperator: "<br/>")
+
+      expect(wrapped_text).to eq("")
+    end
+  end
+
   describe "#truncate" do
     let!(:long_text) { "This is a test string that is longer than thirty characters." }
 
