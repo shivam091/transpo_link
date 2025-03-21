2007fd54cbd11b9f0b7eb581034353a46648a2af
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-21 13:34:19 +0530

    refactor(units): separated translations of `units`

---
+++ config/locales/en/measurement_units.rb
@@ -0,0 +1,231 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+{
+  en: {
+    measurement_units: {
+      categories: {
+        area: "Area based",
+        count: "Count based",
+        weight: "Weight based",
+        volume: "Volume based",
+        length: "Length based",
+      },
+      sub_categories: {
+        cm²: "Square centimetre (cm²)",
+        m²: "Square metre (m²)",
+        km²: "Square kilometre (km²)",
+        in²: "Square inch (in²)",
+        ft²: "Square foot (ft²)",
+        yd²: "Square yard (yd²)",
+        ac: "Acre (ac)",
+        ha: "Hectare (ha)",
+        mg: "Milligramme (mg)",
+        g: "Gramme (g)",
+        kg: "Kilogramme (kg)",
+        q: "Quintal (q)",
+        t: "Ton (t)",
+        lb: "Pound (lb)",
+        oz: "Ounce (oz)",
+        ml: "Millilitre (ml)",
+        L: "Litre (L)",
+        item: "Item",
+        pack: "Pack",
+        box: "Box",
+        carton: "Carton",
+        pallet: "Pallet",
+        bundle: "Bundle",
+        dz: "Dozen",
+        case: "Case",
+        roll: "Roll",
+        cm³: "Cubic centimetre (cm³)",
+        m³: "Cubic metre (m³)",
+        in³: "Cubic inch (in³)",
+        ft³: "Cubic foot (ft³)",
+        gal: "Gallon (gal)",
+        pt: "Pint (pt)",
+        qt: "Quart (qt)",
+        bbl: "Barrel (bbl)",
+        mm: "Millimetre (mm)",
+        cm: "Centimetre (cm)",
+        m: "Metre (m)",
+        km: "Kilometre (km)",
+        in: "Inch (in)",
+        ft: "Foot (ft)",
+        yd: "Yard (yd)",
+        mi: "Mile (mi)",
+      },
+      display_labels: {
+        cm²: {
+          one: "1 sq. centimetre",
+          other: "%{count} sq. centimetres",
+        },
+        m²: {
+          one: "1 sq. metre",
+          other: "%{count} sq. metres",
+        },
+        km²: {
+          one: "1 sq. kilometre",
+          other: "%{count} sq. kilometres",
+        },
+        in²: {
+          one: "1 sq. inch",
+          other: "%{count} sq. inches",
+        },
+        ft²: {
+          one: "1 sq. foot",
+          other: "%{count} sq. feet",
+        },
+        yd²: {
+          one: "1 sq. yard",
+          other: "%{count} sq. yards",
+        },
+        ac: {
+          one: "1 acre",
+          other: "%{count} acres",
+        },
+        ha: {
+          one: "1 hectare",
+          other: "%{count} hectares",
+        },
+        mg: {
+          one: "1 milligramme",
+          other: "%{count} milligrammes",
+        },
+        g: {
+          one: "1 gramme",
+          other: "%{count} grammes",
+        },
+        kg: {
+          one: "1 kilogramme",
+          other: "%{count} kilogrammes",
+        },
+        q: {
+          one: "1 quintal",
+          other: "%{count} quintals",
+        },
+        t: {
+          one: "1 ton",
+          other: "%{count} tons",
+        },
+        lb: {
+          one: "1 pound",
+          other: "%{count} pounds",
+        },
+        oz: {
+          one: "1 ounce",
+          other: "%{count} ounces",
+        },
+        ml: {
+          one: "1 millilitre",
+          other: "%{count} millilitres",
+        },
+        L: {
+          one: "1 litre",
+          other: "%{count} litres",
+        },
+        item: {
+          one: "1 item",
+          other: "%{count} items"
+        },
+        pack: {
+          one: "1 pack",
+          other: "%{count} packs"
+        },
+        box: {
+          one: "1 box",
+          other: "%{count} boxes"
+        },
+        carton: {
+          one: "1 carton",
+          other: "%{count} cartons"
+        },
+        pallet: {
+          one: "1 pallet",
+          other: "%{count} pallets"
+        },
+        bundle: {
+          one: "1 bundle",
+          other: "%{count} bundles"
+        },
+        dz: {
+          one: "1 dozen",
+          other: "%{count} dozens"
+        },
+        case: {
+          one: "1 case",
+          other: "%{count} cases"
+        },
+        roll: {
+          one: "1 role",
+          other: "%{count} roles"
+        },
+        cm³: {
+          one: "1 cu. centimetre",
+          other: "%{count} cu. centimetres",
+        },
+        m³: {
+          one: "1 cu. metre",
+          other: "%{count} cu. metres",
+        },
+        in³: {
+          one: "1 cu. inch",
+          other: "%{count} cu. inches",
+        },
+        ft³: {
+          one: "1 cu. foot",
+          other: "%{count} cu. feet",
+        },
+        gal: {
+          one: "1 gallon",
+          other: "%{count} gallons",
+        },
+        pt: {
+          one: "1 pint",
+          other: "%{count} pints",
+        },
+        qt: {
+          one: "1 quart",
+          other: "%{count} quarts",
+        },
+        bbl: {
+          one: "1 barrel",
+          other: "%{count} barrels",
+        },
+        mm: {
+          one: "1 millimetre",
+          other: "%{count} millimetres",
+        },
+        cm: {
+          one: "1 centimetre",
+          other: "%{count} centimetres",
+        },
+        m: {
+          one: "1 metre",
+          other: "%{count} metres",
+        },
+        km: {
+          one: "1 kilometre",
+          other: "%{count} kilometres",
+        },
+        in: {
+          one: "1 inch",
+          other: "%{count} inches",
+        },
+        ft: {
+          one: "1 foot",
+          other: "%{count} feet",
+        },
+        yd: {
+          one: "1 yard",
+          other: "%{count} yards",
+        },
+        mi: {
+          one: "1 mile",
+          other: "%{count} miles",
+        },
+      },
+    }
+  }
+}
