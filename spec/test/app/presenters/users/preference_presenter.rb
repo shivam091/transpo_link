6e39fec0aa966fffc74a700d6be1ac0c2a87ed31
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-25 22:22:16 +0530

    perf: removed unneeded tests

--- spec/test/app/presenters/users/preference_presenter.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-# -*- frozen_string_literal: true -*-
-# -*- warn_indent: true -*-
-
-class Users::PreferencePresenter < ApplicationPresenter
-  presents :user_preference
-end
