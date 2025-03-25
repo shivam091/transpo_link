4aeb96f843d45032fc06e20f7d64d375f553b9c2
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-25 22:18:11 +0530

    style(views): readable formatting for views

---
+++ spec/test/spec/services/mailer/email_service_spec.rb
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/mailer/email_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Mailer::EmailService, type: :service do
+  context "doing something" do
+    describe ".call" do
+      it "should test something" do
+        skip
+      end
+    end
+  end
+end
