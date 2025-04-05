fdec128328e3d422c10f95045eb4f1f7e8789dcc
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-05 22:17:43 +0530

    feat(po): added service to `cancel` the `po`

---
+++ app/services/purchase_orders/cancel_service.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class PurchaseOrders::CancelService < ApplicationService
+  def initialize(purchase_order)
+    @purchase_order = purchase_order
+  end
+
+  def call
+    cancel_purchase_order
+  end
+
+  private
+
+  attr_reader :purchase_order
+
+  def cancel_purchase_order
+    if purchase_order.may_cancel? && purchase_order.cancel!
+      ServiceResponse.success(payload: {purchase_order:})
+    else
+      ServiceResponse.error(payload: {purchase_order:})
+    end
+  end
+end
