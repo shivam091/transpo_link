bd87f7ea0500c3c248cb9efe8a2d0f9ae093f0a8
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-13 13:17:55 +0530

    fix: corrected breadcrumb path

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -14,6 +14,6 @@ class UnitsController < ApplicationController
   private
 
   def set_breadcrumbs
-    add_breadcrumb t("units.breadcrumb"), unit_path
+    add_breadcrumb t("units.breadcrumb"), units_path
   end
 end
