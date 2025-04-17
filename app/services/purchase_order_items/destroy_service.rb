8aaf4e32cf2eaf03b40750779169b8bf3ade4162
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-17 16:18:12 +0530

    feat(po): added service to `destroy` the po item

---
+++ app/services/purchase_order_items/destroy_service.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class PurchaseOrderItems::DestroyService < ApplicationService
+  def initialize(purchase_order_item)
+    @purchase_order_item = purchase_order_item
+  end
+
+  def call
+    destroy_purchase_order_item
+  end
+
+  private
+
+  attr_reader :purchase_order_item
+
+  def destroy_purchase_order_item
+    if purchase_order_item.destroy
+      ServiceResponse.success(payload: {purchase_order_item:})
+    else
+      ServiceResponse.error(payload: {purchase_order_item:})
+    end
+  end
+end
