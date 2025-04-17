f82b7cb4bdb1a1f3db1f78ce3fb7f540be3083b7
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-17 16:17:57 +0530

    feat(po): added service to `update` the po item

---
+++ app/services/purchase_order_items/update_service.rb
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class PurchaseOrderItems::UpdateService < ApplicationService
+  def initialize(purchase_order_item, purchase_order_item_attributes)
+    @purchase_order_item = purchase_order_item
+    @purchase_order_item_attributes = purchase_order_item_attributes
+  end
+
+  def call
+    update_purchase_order_item
+  end
+
+  private
+
+  attr_reader :purchase_order_item, :purchase_order_item_attributes
+
+  def update_purchase_order_item
+    if purchase_order_item.update(purchase_order_item_attributes)
+      ServiceResponse.success(payload: {purchase_order_item:})
+    else
+      ServiceResponse.error(payload: {purchase_order_item:})
+    end
+  end
+end
