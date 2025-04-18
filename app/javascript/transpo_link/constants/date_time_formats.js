cba125cc4cba1a0c87929b88307934a04b4fe60c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-18 14:27:32 +0530

    feat: use use preferred formats on `live clock`
    Display `date` and `time` in user preferred format on live clock

---
+++ app/javascript/transpo_link/constants/date_time_formats.js
@@ -0,0 +1,15 @@
+export const DATE_FORMATS = {
+  default: "YYYY-MM-DD",
+  short: "D MMM",
+  long: "MMMM D, YYYY",
+  long_with_day: "ddd, MMMM D, YYYY",
+  long_with_hyphen: "YYYY-MM-DD",
+  year_and_month: "YYYY-MM",
+};
+
+export const TIME_FORMATS = {
+  twelve_hours_long: "hh:mm:ss A",
+  twelve_hours_short: "hh:mm A",
+  twenty_four_hours_short: "HH:mm",
+  twenty_four_hours_long: "HH:mm:ss",
+};
