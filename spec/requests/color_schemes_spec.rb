85ed81052d72ea4ec721ddce4d8e735e1d515e7f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-20 19:36:36 +0530

    style(preferences): moved logic to service

--- spec/requests/color_schemes_spec.rb
+++ spec/requests/color_schemes_spec.rb
@@ -10,30 +10,29 @@ RSpec.describe "ColorSchemes", type: :request do
   include_context "sign in as buyer"
 
   describe "PUT|PATCH /color_scheme" do
-    context "when valid color scheme" do
+    context "when valid color scheme is passed" do
       it "updates the user's color scheme and returns success JSON" do
         patch color_scheme_path, params: {color_scheme: "dark"}, as: :json
 
         expect(response).to have_http_status(:ok)
-        expect(buyer.reload.user_preference.preferred_color_scheme).to eq("dark")
-        expect(parsed_response_body["preferred_color_scheme"]).to eq("dark")
+        expect(parsed_response_body["color_scheme"]).to eq("dark")
         expect(parsed_response_body["icon"]).to eq("moon")
       end
     end
 
-    context "when invalid color scheme" do
-      it "returns an unprocessable_entity error" do
+    context "when invalid color scheme is passed" do
+      it "returns an bad_request error" do
         patch color_scheme_path, params: {color_scheme: "rainbow"}, as: :json
 
-        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to have_http_status(:bad_request)
         expect(parsed_response_body["error"]).to eq("Invalid color scheme")
       end
     end
 
-    context "when update fails" do
-      before { allow_any_instance_of(UserPreference).to receive(:update!) { false } }
+    context "when update fails due to validation error" do
+      before { allow_any_instance_of(User).to receive(:update) { false } }
 
-      it "returns an error message" do
+      it "returns an unprocessable_entity error" do
         patch color_scheme_path, params: {color_scheme: "light"}, as: :json
 
         expect(response).to have_http_status(:unprocessable_entity)
