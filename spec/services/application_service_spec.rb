22e69854be8a2359928d9f90776d71f0f3e18489
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-25 11:01:49 +0530

    feat: allow block for service calls

--- spec/services/application_service_spec.rb
+++ spec/services/application_service_spec.rb
@@ -10,19 +10,41 @@ RSpec.describe ApplicationService do
   describe ".call" do
     let(:dummy_service_class) do
       Class.new(ApplicationService) do
+        def initialize(param, &block)
+          @param = param
+          @block = block
+        end
+
         def call
-          :service_result
+          result = :service_result
+          @block.call(@param) if block_given?
+
+          result
         end
       end
     end
 
-    let(:service_instance) { instance_double("DummyService") }
-    let(:result) { dummy_service_class.call }
+    let(:param) { :test_param }
 
-    it "instantiates a new instance of the service class and calls #call on it" do
-      expect(dummy_service_class).to receive(:new) { service_instance }
-      expect(service_instance).to receive(:call) { :service_result }
+    it "instantiates a new instance and calls #call" do
+      service_instance = instance_double(dummy_service_class, call: :service_result)
+
+      expect(dummy_service_class).to receive(:new).with(param) { service_instance }
+      expect(service_instance).to receive(:call)
+
+      result = dummy_service_class.call(param)
       expect(result).to eq(:service_result)
+    end
+
+    it "yields the block if given" do
+      yielded = nil
+
+      result = dummy_service_class.call(param) do |value|
+        yielded = value
+      end
+
+      expect(result).to eq(:service_result)
+      expect(yielded).to eq(param)
     end
   end
 end
