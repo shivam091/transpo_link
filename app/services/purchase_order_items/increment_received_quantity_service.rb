f80ce3fa8e83ccef76bf4ee8407c42baa80079f4
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-04-25 14:36:11 +0530

    feat: service to increment `received_quantity`

---
+++ app/services/purchase_order_items/increment_received_quantity_service.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class PurchaseOrderItems::IncrementReceivedQuantityService < ApplicationService
+  def initialize(purchase_order_item)
+    @purchase_order_item = purchase_order_item
+  end
+
+  def call
+    increment_received_quantity!
+  end
+
+  private
+
+  attr_reader :purchase_order_item
+
+  def increment_received_quantity!
+    if purchase_order_item.increment!(:received_quantity, purchase_order_item.quantity)
+      ServiceResponse.success(payload: {purchase_order_item:})
+    else
+      ServiceResponse.error(payload: {purchase_order_item:})
+    end
+  end
+end
---
+++ spec/services/purchase_order_items/increment_received_quantity_service_spec.rb
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/purchase_order_items/increment_received_quantity_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe PurchaseOrderItems::IncrementReceivedQuantityService, type: :service do
+  let!(:purchase_order_item) { create(:purchase_order_item, quantity: 5) }
+
+  subject(:service_response) { described_class.(purchase_order_item) }
+
+  describe ".call" do
+    context "when incrementing is successful" do
+      it "increments the received_quantity" do
+        expect {
+          service_response
+        }.to change { purchase_order_item.reload.received_quantity }.by(5)
+      end
+
+      include_examples "returns a success response"
+    end
+
+    context "when incrementing is unsuccessful" do
+      before { allow(purchase_order_item).to receive(:increment!) { false } }
+
+      it "does not increment the received_quantity" do
+        expect {
+          service_response
+        }.to not_change { purchase_order_item.reload.received_quantity }
+      end
+
+      include_examples "returns an error response"
+    end
+  end
+end
