a43fb9f4886544833d448a92c84b3d84e60c2b1b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-05-07 18:00:16 +0530

    style: moved translations of `deliveries` form

--- config/locales/en/purchase_order_items.rb
+++ config/locales/en/purchase_order_items.rb
@@ -29,16 +29,6 @@
       },
       show: {
       },
-      deliveries: {
-        new: {
-          title: "Confirm delivery"
-        },
-        form: {
-          select_unit_of_measure: "Select unit of measure",
-          quantity_help_text: "Specify how many quantity of the item you are delivering. This can differ from the ordered quantity if delivering partially.",
-          unit_id_help_text: "Choose the unit in which the quantity is being delivered. Ensure it matches the packaging or measurement you’re using.",
-        },
-      },
     }
   }
 }
