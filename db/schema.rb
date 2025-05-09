4545b5f49d793509ec9d43eec79dd4b041057c26
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-05-09 19:16:33 +0530

    feat: enabled `btree_gist` extension

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_07_113346) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "btree_gist"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
