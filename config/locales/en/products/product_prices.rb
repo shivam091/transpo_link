b534bd8ed679cfafda3bfc4aa090644b3c85cd42
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-05-13 12:52:02 +0530

    style: rearranged product prices section files

--- config/locales/en/products/product_prices.rb
+++ config/locales/en/products/product_prices.rb
@@ -6,6 +6,11 @@
   en: {
     products: {
       product_prices: {
+        index: {
+        },
+        product_price: {
+          delete_confirmation_text: "Confirm deletion of this price tier. Prices based on this tier will no longer be applied.",
+        },
         new: {
           title: "New product price",
         },
@@ -13,6 +18,13 @@
           title: "Edit product price",
         },
         form: {
+          tabular_view: {
+          },
+          tabular_field: {
+            select_warehouse: "Select warehouse",
+            select_currency: "Select currency",
+            select_unit: "Select unit",
+          },
           modal_view: {
             select_warehouse: "Select warehouse",
             select_currency: "Select currency",
