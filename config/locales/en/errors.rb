bcd8553a7cb7f599083125ef9f5d6f221745f846
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-05-17 15:46:13 +0530

    feat: added error page for `404`

--- config/locales/en/errors.rb
+++ config/locales/en/errors.rb
@@ -10,6 +10,12 @@
       purchase_orders: {
         missing_inventory: 'Inventory is missing for the product "%{product_name}" in the warehouse "%{warehouse_name}".',
       },
+      not_found: {
+        title: "Page not found",
+        not_found: "Oops! Page not found",
+        error_primary_text: "The page or resource you were looking for doesn't exist.",
+        error_secondary_text: "You may have mistyped the address or the page may have moved permanently to new web address. You can either return to previous page, visit the home page, or contact the administrator at %{admin_email}",
+      },
     }
   }
 }
