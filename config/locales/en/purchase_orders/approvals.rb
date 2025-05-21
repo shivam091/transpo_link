36910d180607c1ccebdbeedd670354f24c6429db
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-05-21 14:48:17 +0530

    feat: added new attributes on `approval` form

--- config/locales/en/purchase_orders/approvals.rb
+++ config/locales/en/purchase_orders/approvals.rb
@@ -13,6 +13,8 @@
           approval_note: "Please enter below information to confirm approval of %{reference_code}.",
           select_incoterm_code: "Select incoterm code",
           select_shipping_method: "Select shipping method",
+          incoterm_code_help_text: "Select the delivery agreement between you (the supplier) and the warehouse (the buyer) that defines who is responsible for shipping, insurance, customs clearance, and final delivery.",
+          shipping_method_help_text: "Expected shipping mode agreed during PO approval; e.g., SEA, ROAD (might differ from actual)",
         },
       }
     }
